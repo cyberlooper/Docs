969f6e50d0ec71f13fe77b4430aa3488573edd89
MagicalCodeMonkey <MagicalCodeMonkey@users.noreply.github.com>
2019-06-12 20:00:57 -0400

    Update wait_loop.sh
    Remove "tail" from the outputs

--- wait_loop.sh
+++ wait_loop.sh
@@ -2,17 +2,17 @@ while (true); do
     clear
     date
     echo "-- APT --"
-    ps -ef | grep apt | grep -v grep && printf "\n" || APT_DONE=1
+    ps -ef | grep apt | grep -v tail | grep -v grep && printf "\n" || APT_DONE=1
     if [[ ${APT_DONE:-} = 1 ]]; then
         echo "No apt processes remaining"
     fi
     echo "-- UPDATE --"
-    ps -ef | grep update | grep -v grep && printf "\n" || UPDATE_DONE=1
+    ps -ef | grep update | grep -v tail | grep -v grep && printf "\n" || UPDATE_DONE=1
     if [[ ${UPDATE_DONE:-} = 1 ]]; then
         echo "No update processes remaining"
     fi
     echo "-- UPGRADE --"
-    ps -ef | grep upgrade | grep -v grep || UPGRADE_DONE=1
+    ps -ef | grep upgrade | grep -v tail | grep -v grep || UPGRADE_DONE=1
     if [[ ${UPGRADE_DONE:-} = 1 ]]; then
         echo "No upgrade processes remaining"
     fi
