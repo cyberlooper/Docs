cc9aa2b1237fd8445927143026d48be847bb91d3
MagicalCodeMonkey <MagicalCodeMonkey@users.noreply.github.com>
2019-06-28 18:11:06 -0400

    Update wait_loop.sh
    Should make the output of this a little clearer

--- wait_loop.sh
+++ wait_loop.sh
@@ -1,26 +1,24 @@
+start=$(date)
 while (true); do
     clear
-    date
-    echo "-- APT --"
-    ps -ef | grep apt | grep -v tail | grep -v grep && printf "\n" || APT_DONE=1
-    if [[ ${APT_DONE:-} = 1 ]]; then
-        echo "No apt processes remaining"
-    fi
-    echo "-- UPDATE --"
-    ps -ef | grep update | grep -v tail | grep -v shellinabox | grep -v grep && printf "\n" || UPDATE_DONE=1
-    if [[ ${UPDATE_DONE:-} = 1 ]]; then
-        echo "No update processes remaining"
-    fi
-    echo "-- UPGRADE --"
-    ps -ef | grep upgrade | grep -v tail | grep -v shellinabox | grep -v unattended-upgrade | grep -v grep || UPGRADE_DONE=1
-    if [[ ${UPGRADE_DONE:-} = 1 ]]; then
-        echo "No upgrade processes remaining"
-    fi
+    echo "Started: ${start}"
+    echo "Now:     $(date)"
+    AP_COUNT=$(ps -ef | grep apt | grep -v tail | grep -v grep | wc -l)
+    echo " - Apt processes remaining: ${AP_COUNT}"
 
-    if [[ ${APT_DONE:-} = 1 && ${UPDATE_DONE:-} = 1 && ${UPGRADE_DONE:-} = 1 ]]; then
-        clear
-        echo "OpenFLIXR is probably ready for the next step!"
+    UPDATE_COUNT=$(ps -ef | grep update | grep -v tail | grep -v shellinabox | grep -v grep | wc -l)
+    echo " - Update processes remaining: ${UPDATE_COUNT}"
+
+    UPGRADE_COUNT=$(ps -ef | grep upgrade | grep -v tail | grep -v shellinabox | grep -v unattended-upgrade | grep -v grep | wc -l)
+    echo " - Upgrade processes remaining: ${UPGRADE_COUNT}"
+
+    if [[ ${AP_COUNT:-} = 0 && ${UPDATE_COUNT:-} = 0 && ${UPGRADE_COUNT:-} = 0 ]]; then
+        echo "|------------------------------------------------|"
+        echo "| OpenFLIXR is probably ready for the next step! |"
+        echo "|------------------------------------------------|"
         exit
+    else
+        echo "> Keep waiting <"
     fi
 
     sleep 1;
