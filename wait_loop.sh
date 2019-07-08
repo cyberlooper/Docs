9a2c0dc063aebeb58a401f49c661b7babf8cc34e
MagicalCodeMonkey <mwoodru@gmail.com>
2019-07-08 17:36:57 -0400

    Now with more durations! Make the wait loop watch for duration since a change in process count

--- wait_loop.sh
+++ wait_loop.sh
@@ -1,25 +1,74 @@
-start=$(date)
+start=$(date +%s)
+start_display=$(date)
+duration=""
+elapsed_minutes="none"
+APT_COUNT_LAST=0
+UPDATE_COUNT_LAST=0
+UPGRADE_COUNT_LAST=0
+WAIT_TIME=5
 while (true); do
     clear
-    echo "Started: ${start}"
+    elapsed=$(($(date +%s)-$start))
+    duration=$(date -ud @$elapsed +'%M minutes %S seconds')
+    echo "Started: ${start_display}"
     echo "Now:     $(date)"
-    AP_COUNT=$(ps -ef | grep apt | grep -v tail | grep -v grep | wc -l)
-    echo " - Apt processes remaining: ${AP_COUNT}"
+    echo "Elapsed: ${duration}"
+    APT_COUNT=$(ps -ef | grep apt | grep -v tail | grep -v grep | wc -l)
+    echo " - Apt processes remaining: ${APT_COUNT}"
+    if [[ ${APT_COUNT} != 0 && ${APT_COUNT_LAST_ELAPSED:-} != "" ]]; then
+        echo "   Last changed: $(date -ud @${APT_COUNT_LAST_ELAPSED} +'%M minutes %S seconds')"
+    else
+        echo ""
+    fi
 
     UPDATE_COUNT=$(ps -ef | grep update | grep -v tail | grep -v shellinabox | grep -v grep | wc -l)
     echo " - Update processes remaining: ${UPDATE_COUNT}"
+    if [[ ${UPDATE_COUNT} != 0 && ${UPDATE_COUNT_LAST_ELAPSED:-} != "" ]]; then
+        echo "   Last changed: $(date -ud @${UPDATE_COUNT_LAST_ELAPSED} +'%M minutes %S seconds')"
+    else
+        echo ""
+    fi
 
     UPGRADE_COUNT=$(ps -ef | grep upgrade | grep -v tail | grep -v shellinabox | grep -v unattended-upgrade | grep -v grep | wc -l)
     echo " - Upgrade processes remaining: ${UPGRADE_COUNT}"
+    if [[ ${UPGRADE_COUNT} != 0 && ${UPGRADE_COUNT_LAST_ELAPSED:-} != "" ]]; then
+        echo "   Last changed: $(date -ud @${UPGRADE_COUNT_LAST_ELAPSED} +'%M minutes %S seconds')"
+    else
+        echo ""
+    fi
 
-    if [[ ${AP_COUNT:-} = 0 && ${UPDATE_COUNT:-} = 0 && ${UPGRADE_COUNT:-} = 0 ]]; then
+    if [[ ${APT_COUNT} = 0 && ${UPDATE_COUNT} = 0 && ${UPGRADE_COUNT} = 0 ]]; then
         echo "|------------------------------------------------|"
         echo "| OpenFLIXR is probably ready for the next step! |"
         echo "|------------------------------------------------|"
         exit
+    elif [[ ${APT_COUNT_LAST_ELAPSED_MINUTES#0} -ge ${WAIT_TIME} || ${UPDATE_COUNT_LAST_ELAPSED_MINUTES#0} -ge ${WAIT_TIME} || ${UPGRADE_COUNT_LAST_ELAPSED#0} -ge ${WAIT_TIME} ]]; then
+        echo "> It has been more than ${WAIT_TIME} minutes since at least one of the above changed..."
+        echo "> You might want to consider rebooting and running this script again"
     else
-        echo "> Keep waiting <"
+        echo "> Keep waiting..."
+    fi
+#echo "**** WAIT_TIME=${WAIT_TIME}"
+    if [[ ${APT_COUNT} != ${APT_COUNT_LAST} ]]; then
+        APT_COUNT_LAST=${APT_COUNT}
+        APT_COUNT_LAST_ELAPSED=$(($(date +%s)-$start))
+        APT_COUNT_LAST_ELAPSED_MINUTES=$(date -ud @${APT_COUNT_LAST_ELAPSED} +%M)
+        #echo "**** APT_COUNT_LAST_ELAPSED_MINUTES=${APT_COUNT_LAST_ELAPSED_MINUTES#0}"
     fi
 
-    sleep 1;
+    if [[ ${UPDATE_COUNT} != ${UPDATE_COUNT_LAST} ]]; then
+        UPDATE_COUNT_LAST=${APT_COUNT}
+        UPDATE_COUNT_LAST_ELAPSED=$(($(date +%s)-$start))
+        UPDATE_COUNT_LAST_ELAPSED_MINUTES=$(date -ud @${UPDATE_COUNT_LAST_ELAPSED} +%M)
+        #echo "**** UPDATE_COUNT_LAST_ELAPSED_MINUTES=${UPDATE_COUNT_LAST_ELAPSED_MINUTES#0}"
+    fi
+
+    if [[ ${UPGRADE_COUNT} != ${UPGRADE_COUNT_LAST} ]]; then
+        UPGRADE_COUNT_LAST=${APT_COUNT}
+        UPGRADE_COUNT_LAST_ELAPSED=$(($(date +%s)-$start))
+        UPGRADE_COUNT_LAST_ELAPSED_MINUTES=$(date -ud @${UPGRADE_COUNT_LAST_ELAPSED} +%M)
+        #echo "**** UPGRADE_COUNT_LAST_ELAPSED_MINUTES=${UPGRADE_COUNT_LAST_ELAPSED_MINUTES#0}"
+    fi
+
+    sleep 5;
 done
